3bd65f6b7df1553bfa419607d38b2c0c44b2720d
Julian <yg1811@hotmail.com>
2018-06-08 23:03:26 -0700

    cut down length of task name

--- baselines/gail/run_mujoco.py
+++ baselines/gail/run_mujoco.py
@@ -62,9 +62,9 @@ def get_task_name(args):
     if args.traj_limitation != np.inf:
         task_name += "transition_limitation_%d." % args.traj_limitation
     task_name += args.env_id.split("-")[0]
-    task_name = task_name + ".g_step_" + str(args.g_step) + ".d_step_" + str(args.d_step) + \
-        ".policy_entcoeff_" + str(args.policy_entcoeff) + ".adversary_entcoeff_" + str(args.adversary_entcoeff)
-    task_name += ".seed_" + str(args.seed)
+    # task_name = task_name + ".g_step_" + str(args.g_step) + ".d_step_" + str(args.d_step) + \
+    #     ".policy_entcoeff_" + str(args.policy_entcoeff) + ".adversary_entcoeff_" + str(args.adversary_entcoeff)
+    # task_name += ".seed_" + str(args.seed)
     return task_name
 
 
@@ -178,7 +178,6 @@ def visualizer(env, policy_func, load_model_path, timesteps_per_batch, number_tr
     # acs_list = []
     # len_list = []
     # ret_list = []
-    print('visualzer called')
     for _ in tqdm(range(number_trajs)):
       ob = env.reset()
       total_rew = 0
