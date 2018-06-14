da0ff4d35b3a4feb82b5dce657a5cccd5652fb94
jonathan <jaustinb@stanford.edu>
2018-06-13 23:05:59 -0700

    fix params

--- baselines/gail/ppo_mpi.py
+++ baselines/gail/ppo_mpi.py
@@ -387,17 +387,19 @@ def learn(env, policy_func, reward_giver, expert_dataset, rank,
         ob_expert, ac_expert = expert_dataset.get_next_batch(len(ob))
         batch_size = len(ob) // d_step
         d_losses = []  # list of tuples, each of which gives the loss for a minibatch
-        for ob_batch, ac_batch in dataset.iterbatches((ob, ac),
-                                                      include_final_partial_batch=False,
-                                                      batch_size=batch_size):
-            ob_expert, ac_expert = expert_dataset.get_next_batch(len(ob_batch))
-            # update running mean/std for reward_giver
-            ob_batch = ob_batch[:, -ob_expert.shape[1]:][:-30]
-            if hasattr(reward_giver, "obs_rms"): reward_giver.obs_rms.update(np.concatenate((ob_batch, ob_expert), 0)[:, :-30])
-            # *newlosses, g = reward_giver.lossandgrad(ob_batch, ac_batch, ob_expert, ac_expert)
-            *newlosses, g = reward_giver.lossandgrad(ob_batch[:, :-30], ob_expert[:, :-30])
-            d_adam.update(allmean(g), d_stepsize)
-            d_losses.append(newlosses)
+
+        for _ in range(optim_epochs // 10):
+            for ob_batch, ac_batch in dataset.iterbatches((ob, ac),
+                                                          include_final_partial_batch=False,
+                                                          batch_size=batch_size):
+                ob_expert, ac_expert = expert_dataset.get_next_batch(len(ob_batch))
+                # update running mean/std for reward_giver
+                ob_batch = ob_batch[:, -ob_expert.shape[1]:][:-30]
+                if hasattr(reward_giver, "obs_rms"): reward_giver.obs_rms.update(np.concatenate((ob_batch, ob_expert), 0)[:, :-30])
+                # *newlosses, g = reward_giver.lossandgrad(ob_batch, ac_batch, ob_expert, ac_expert)
+                *newlosses, g = reward_giver.lossandgrad(ob_batch[:, :-30], ob_expert[:, :-30])
+                d_adam.update(allmean(g), d_stepsize)
+                d_losses.append(newlosses)
         logger.log(fmt_row(13, np.mean(d_losses, axis=0)))
 
 
